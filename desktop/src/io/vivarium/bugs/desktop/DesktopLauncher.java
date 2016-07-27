16d1bb5c1920566f61f437288f54feb762a6b6c8
John H. Uckele <j@vivarium.io>
2016-07-26 21:41:55 -0400

    Initial project as created by gdx-setup.jar (version 1.9.3)
    Name: BrokenGWTDemo
    Package: io.vivarium.bugs
    Game class: BrokwnGWTDemo
    Sub Projects: Desktop and Html

---
+++ desktop/src/io/vivarium/bugs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.vivarium.bugs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.vivarium.bugs.BrokenGWTDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BrokenGWTDemo(), config);
+	}
+}
